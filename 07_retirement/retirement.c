6262f2a9e6c91c43b77710ba76ae605366fb4c37
Coursera Learner <nobody@nowhere.nul>
2020-12-20 10:13:05 +0000

    Did assignmnet 07_retirement

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -14,9 +14,9 @@ double calculate(double initial, retire_info info, int startAge)
   int i;
   
   final= initial;
-  for(i=0;i<info.months,i++)
+  for(i=0;i<info.months;i++)
     {
-      printf("Age %3d month %2d  you have $%.2lf\n",startAge/12, startAge%12, res);
+      printf("Age %3d month %2d  you have $%.2lf\n",startAge/12, startAge%12, final);
       final += final*(info.rate_of_return/12+info.contribution);
       startAge++;
     }
@@ -25,12 +25,26 @@ double calculate(double initial, retire_info info, int startAge)
 void retirement( int startAge, double initial, retire_info working, retire_info retired)
 {
   double worked;
-  worked= calculate(initial, working, starAge);
+  worked= calculate(initial, working, startAge);
   calculate(worked, retired, startAge+working.months);
   
 }
 
 int main()
 {
-  int 
+
+  retire_info working,retired;
+  working.months= 489;
+  working.contribution=1000;
+  working.rate_of_return= 0.045;
+
+  retired.months= 384;
+  retired.contribution= -4000;
+  retired.rate_of_return= 0.01;
+
+
+  retirement(327, 21345, working, retired);
+
+  return 0;
+
 }
